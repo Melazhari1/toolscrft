fa9490de06dd74799515b282aa91a2a6171d99a3
Melazhari <elazharimohamedp@gmail.com>
2026-04-25 14:25:50 +0100

    fix: use dynamic tool list on homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,15 +9,7 @@ class HomeController extends Controller
     {
         return Inertia::render('Home', [
             'seo' => $this->generateSeo('Professional local SEO tools and data utilities', 'Access a suite of privacy-focused SEO and developer utilities for text analysis, domain data, placeholder text, and JSON conversion in a fast local interface.'),
-            'tools' => [
-                ['name' => 'Textalyzer', 'slug' => 'textalyzer.index', 'desc' => 'Word count & keyword density'],
-                ['name' => 'Domain Expiry', 'slug' => 'domain-expiry.index', 'desc' => 'Check domain expiration dates'],
-                ['name' => 'Whois Lookup', 'slug' => 'domain-whois.index', 'desc' => 'Domain registration data'],
-                ['name' => 'Lorem Ipsum', 'slug' => 'lorem-ipsum.index', 'desc' => 'JS text generator'],
-                ['name' => 'JSON to CSV', 'slug' => 'json-to-csv.index', 'desc' => 'JSON to CSV file conversion'],
-                ['name' => 'CSV to JSON', 'slug' => 'csv-to-json.index', 'desc' => 'CSV to JSON conversion'],
-                ['name' => 'URL Encoder', 'slug' => 'url-encoder.index', 'desc' => 'Encode/decode URLs']
-            ]
+            'tools' => $this->getTools()
         ]);
     }
 }
